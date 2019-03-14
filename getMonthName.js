59e6a715cef917ba6fb1bc5bbc433ee820c54d86
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2019-03-14 16:02:30 +0100

    Adding new function getMonthName

---
+++ getMonthName.js
@@ -0,0 +1,20 @@
+function getMonthName(place) {
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+
+    return months[place];
+}
+
+
